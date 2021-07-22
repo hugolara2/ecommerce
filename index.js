2b15f7ee855dca00909aa5e173d989dbfbb5d147
Hugo Lara <hugo.alex200197@outlook.com>
2021-07-21 22:55:44 -0500

    implementacion de las rutas en index asi como de las vistas

---
+++ index.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const path = require('path'); 
+const app = express();
+const port = 3000;
+const productsRouter = require('./routes/products');
+
+app.use("/static", express.static(path.join(__dirname, "public")));
+
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "pug");
+
+app.use('/products', productsRouter);
+
+app.listen(port, () => {
+  console.log(`Application listening at http://localhost:${port}`);
+});
