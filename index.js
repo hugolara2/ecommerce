b413f6792809a58808f9b04c97597f9d8f09c5f0
Hugo Lara <hugo.alex200197@outlook.com>
2021-07-22 22:46:27 -0500

    created views at folder, products moved in

--- index.js
+++ index.js
@@ -1,22 +1,34 @@
 const express = require('express');
-const path = require('path'); 
-const app = express();
-const port = 3000;
+const path = require('path');
+const bodyParser = require('body-parser');
 const productsRouter = require('./routes/products');
 const productApiRouter = require('./routes/api/products');
-const bodyParser = require('body-parser');
 
+//app
+const app = express();
+
+//Port
+const port = 3000;
+
+//Static files
 app.use("/static", express.static(path.join(__dirname, "public")));
 
+//View engine setup
 app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "pug");
 
-//Views
+app.use(bodyParser.json());
+
+//Routes
 app.use('/products', productsRouter);
 app.use("/api/products", productApiRouter);
 
-app.use(bodyParser.json());
+//Redirect
+app.get('/', (req, res) => {
+  res.redirect('/products');
+});
 
+//Server
 app.listen(port, () => {
   console.log(`Application listening at http://localhost:${port}`);
 });
